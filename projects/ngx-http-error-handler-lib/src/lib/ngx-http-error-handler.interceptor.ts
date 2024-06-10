bbbbd1697de9974f234940fa7681688fb8f8421a
MD Bayzed Meer <bayzed.meer@brainstation-23.com>
2024-06-10 15:17:49 +0600

    feat: add http error interceptor

---
+++ projects/ngx-http-error-handler-lib/src/lib/ngx-http-error-handler.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class NgxHttpErrorInterceptor implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error: unknown) => {
+        console.log('Error sent from HttpErrorInterceptor:', error);
+        return throwError(() => error);
+      })
+    );
+  }
+}
